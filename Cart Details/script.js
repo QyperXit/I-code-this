893d47875aa934e5d52981fb273abb906399ef2a
QyperXit <67711154+QyperXit@users.noreply.github.com>
2023-11-18 00:54:56 +0000

    cart details
    shopping cart detail build

---
+++ Cart Details/script.js
@@ -0,0 +1,23 @@
+// Add JavaScript code here
+
+let input = document.getElementById('num');
+let minus = document.querySelector('.fa-minus');
+let plus = document.querySelector('.fa-plus');
+
+let value = 2;
+
+input.value = value;
+
+// Add event listeners for plus and minus buttons
+minus.addEventListener('click', function () {
+    if (value > 0) {
+        value--;
+        input.value = value;
+    }
+});
+
+plus.addEventListener('click', function () {
+    value++;
+    input.value = value;
+});
+
